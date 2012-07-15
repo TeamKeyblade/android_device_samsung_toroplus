00fda281f27f57c9e1c7e32c1af3a5ccdce5e206
Robert Burns <burnsra@gmail.com>
2012-07-14 23:04:23 -0400

    Update for JellyBean
    Change-Id: I9a20c01e599ef28ae3ed801634eddabd8f444984

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,11 @@
 # Use the non-open-source part, if present
 -include vendor/samsung/toroplus/BoardConfigVendor.mk
 
-# Use the part that is common between all toros
+# Use the open-source board-info file if none was set by the vendor
+# config.
+TARGET_BOARD_INFO_FILE ?= device/samsung/toroplus/board-info.txt
+
+# Use the part that is common between all tunas
 include device/samsung/tuna/BoardConfig.mk
 
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 30691799040
--- full_toroplus.mk
+++ full_toroplus.mk
@@ -21,16 +21,20 @@
 # lines, full and toro, hence its name.
 #
 
-# Camera
 PRODUCT_PACKAGES := \
+<<<<<<< HEAD
     Camera \
     Gallery
+=======
+    Gallery \
+    GNexusParts
+>>>>>>> 6355269... Update for JellyBean
 
 #if we do this after the full_base_telephony is included some of these don't get picked up..
 PRODUCT_COPY_FILES += \
     device/samsung/toroplus/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
     device/samsung/toroplus/plus-apns-conf.xml:system/etc/apns-conf.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
